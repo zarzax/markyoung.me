e3d82e3a2741e789c1169b797f6420005ecd5e53
Mark Young <mark@zarzax.com>
2009-10-14 13:19:02 -0500

    ran scaffold on Project model

---
+++ db/migrate/20091014181829_create_projects.rb
@@ -0,0 +1,18 @@
+class CreateProjects < ActiveRecord::Migration
+  def self.up
+    create_table :projects do |t|
+      t.string :title
+      t.text :teaser
+      t.text :body
+      t.string :icon_url
+      t.string :source_url
+      t.string :permalink
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :projects
+  end
+end
